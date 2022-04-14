b93de0ca237c088d418a06d56b0cecb96de7b9d5
takashi <yagita.takashi@gmail.com>
2022-04-14 19:18:33 +0900

    first commit

--- tests/unit/test_argocd_eks_stack.py
+++ tests/unit/test_argocd_eks_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from argocd_eks.argocd_eks_stack import ArgocdEksStack
+from _stacks.eks_cluster_stack import EksClusterStack
 
 # example tests. To run these tests, uncomment this file along with the example
-# resource in argocd_eks/argocd_eks_stack.py
+# resource in _stacks/eks_cluster_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = ArgocdEksStack(app, "argocd-eks")
+    stack = EksClusterStack(app, "argocd-eks")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
